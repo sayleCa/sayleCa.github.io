7fe0cb7422c46ec3611b86ed39c83a5cfc4f6790
sayleCa <sayleuniversidad@gmail.com>
2026-09-21 13:13:12 -0600

    Delete js directory

--- js/script.js
+++
@@ -1,8 +0,0 @@
-const toggle = document.querySelector(".menu-toggle");
-const nav = document.querySelector(".nav");
-if (toggle && nav) {
-  toggle.addEventListener("click", () => {
-    const open = nav.classList.toggle("open");
-    toggle.setAttribute("aria-expanded", open);
-  });
-}
