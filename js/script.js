ac5f22f4edda5d77a82e2b35af24e997b5c5861d
sayleCa <sayleuniversidad@gmail.com>
2026-09-21 13:06:16 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-
+const toggle = document.querySelector(".menu-toggle");
+const nav = document.querySelector(".nav");
+if (toggle && nav) {
+  toggle.addEventListener("click", () => {
+    const open = nav.classList.toggle("open");
+    toggle.setAttribute("aria-expanded", open);
+  });
+}
